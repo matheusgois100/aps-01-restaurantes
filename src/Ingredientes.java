d997eb307689a4a27527cb93f5fe51eea67158ed
Matheus Gois <matheusgoois@gmail.com>
2017-09-11 23:08:54 -0300

    classe ingredientes

---
+++ src/Ingredientes.java
@@ -0,0 +1,10 @@
+
+public class Ingredientes {
+	public static String ingrediente1 = "Ervilha";
+	public static String ingrediente2 = "Milho";
+	public static String ingrediente3 = "Orégano";
+	public static String ingrediente4 = "Palmito";
+	public static String ingrediente5 = "Pepino";
+	
+
+}
