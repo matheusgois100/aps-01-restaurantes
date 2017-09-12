b2c8fe4caf658fa4edc4b844f634d29b89b3ad5b
Matheus Gois <matheusgoois@gmail.com>
2017-09-11 23:05:00 -0300

    classe sistema de pedidos adicionada

---
+++ src/SistemaDePedidos.java
@@ -0,0 +1,10 @@
+
+public class SistemaDePedidos {
+	public static void main(String[] args){
+		Restaurante restaurante = new Restaurante();
+		restaurante.mostrarCardapio();
+		System.out.println("=====================================");
+		restaurante.mostraIng();
+	}
+
+}
